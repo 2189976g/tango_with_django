1b4f6fea0528c7f09be6f02ba9c6ddb01a10c51d
Bartlomiej Gorski <2189976g@student.gla.ac.uk>
2017-02-07 15:38:46 +0000

    Completed chapter 8

--- rango/views.py
+++ rango/views.py
@@ -12,7 +12,7 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'> Home</a>")
+    return render(request, 'rango/about.html')
 
 
 def show_category(request, category_name_slug):
